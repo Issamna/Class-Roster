ddfa0af4c428c9caa458d4e7c5be6b62422a5eb2
Issam <issam.n.a@gmail.com>
2020-02-02 12:20:37 -0500

    Initial commit

---
+++ Class Roster/networkStudent.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "networkStudent.h"
+using namespace std;
+
+Degree networkStudent::getDegreeProgram() {
+	return degreeType;
+}
---
+++ Class Roster/softareStudent.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <string>
+#include "softwareStudent.h"
+using namespace std;
+
+Degree softwareStudent::getDegreeProgram() {
+	return degreeType;
+}
